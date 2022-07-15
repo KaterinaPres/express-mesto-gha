0f32d45a44c02db573ccfa4ee3419e88f370469f
Ekaterina Presnyakova <katuwka1@mail.ru>
2022-07-15 18:00:59 +0400

    14pr after code-review

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 const routerUsersMy = require('./routes/users');
 const routerCardsMy = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
-const ForbiddenError = require('./errors/ForbiddenError');
+// const ForbiddenError = require('./errors/ForbiddenError');
 const NotFoundError = require('./errors/NotFoundError');
 const auth = require('./middlewares/auth');
 const { regUrl } = require('./token/MongoError');
@@ -41,30 +41,26 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.use('*', () => {
-  throw new NotFoundError();
-});
 
 app.use(auth);
 
 app.use('/', routerUsersMy);
 app.use('/', routerCardsMy);
 app.use('*', () => {
-  throw new ForbiddenError();
+  throw new NotFoundError('Страница не найдена');
 });
-app.use(errors());
+app.use(errors()); 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-
-  if (statusCode !== 201 || statusCode !== 200) {
-    res
-      .status(statusCode)
-      .send({
-        message,
-      });
-    console.error(err.stack);
-  } else {
-    next(err);
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  if (statusCode === 500) {
+    console.log(err.stack);
   }
 });
 
