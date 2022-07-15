aed78dd3499d6c990df171a553ea7c99db8e2dd5
Ekaterina Presnyakova <katuwka1@mail.ru>
2022-07-15 16:32:05 +0400

    14pr after revu

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const { checkToken } = require('../token/jwt');
+const userMy = require('../models/user');
 const NotAutorization = require('../errors/NotAutorization'); // 401
 
 module.exports = (req, res, next) => {
@@ -9,6 +10,16 @@ module.exports = (req, res, next) => {
   let payload;
   try {
     payload = checkToken(authMy);
+    userMy.findOne({ _id: payload._id })
+      .then((user) => {
+        if (!user) {
+          next(new NotAutorization());
+          return;
+        }
+        req.user = { _id: user._id };
+        next();
+      })
+      .catch(next);
   } catch (err) {
     next(new NotAutorization('Авторизуйтесь для доступа'));
     return;
